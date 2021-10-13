1a9f22f7e12b0b546caa5ff803dd0df9320606be
drfurtado <ovande.furtado@csun.edu>
2021-10-13 13:18:57 -0700

    changed the html format for projects

---
+++ help.R
@@ -0,0 +1,29 @@
+# demo
+https://slides.yihui.org/xaringan/#1
+
+# two columns
+
+.pull-left[
+  - One bullet.
+  
+  - Another bullet.
+]
+
+.pull-right[
+  ![an image](foo.png)
+]
+
+# speaker notes
+
+---
+  
+  The holy passion of Friendship is of so sweet and steady
+and loyal and enduring a nature that it will last through
+a whole lifetime...
+
+???
+  
+  _if not asked to lend money_.
+
+--- Mark Twain
+
